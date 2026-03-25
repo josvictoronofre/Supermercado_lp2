c6fbe597ee584b223c697628495c6cacd0bfc188
Victor <josvictoronofre@gmail.com>
2026-03-25 15:53:13 +0000

    Configuração do projeto seguindo as instruções

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,25 @@
 package main;
 
+import modelo.Produto;
+import modelo.ProdutoAlimenticio;
+import modelo.ProdutoEletronico;
+import modelo.Fornecedor;
+import servico.CarrinhoDeCompras;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Fornecedor fazendaLocal = new Fornecedor("Fazenda Local", "0001" , "Brasil");
+		Fornecedor techGlobal = new Fornecedor("Tech Global", "0002", "Estados Unidos");
+		
+		Produto banana = new ProdutoAlimenticio("Banana", 2341, 7, fazendaLocal, 6);
+		Produto fone = new ProdutoEletronico("Fone de Ouvido", 3323, 150, techGlobal, 12);
+		
+		CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+		carrinho.adicionarItem(banana);
+		carrinho.adicionarItem(fone);
+		
+		System.out.printf("Valor total: %.2f\n", carrinho.processarCompra());
 	}
 
 }
